00c4a46e61c28bf8010f6a014309eac6044894bf
mattiavisona <mattiav2706@gmail.com>
2023-02-22 01:00:31 +0100

    comment of the exercise

---
+++ script.js
@@ -0,0 +1,33 @@
+
+
+// Prima parte dell'esercizio
+
+/*
+- Chiedere all'utente il suo nome
+- Memorizzarlo in una variabile
+- Stamparlo in pagina
+*/
+
+// Seconda parte
+
+/*
+- Chiedere all'utente il suo cognome
+- Memorizzarlo in una variabile
+- Stamparlo in pagina
+*/
+
+// Terza parte
+
+
+/*
+- Chiedere all'utente il suo colore preferito
+- Memorizzarlo in una variabile
+- Stamparlo in pagina
+*/
+
+/*
+Scriviamo in pagina la concatenazione delle variabili con un numero 
+scelto da noi
+*/ 
+
+
